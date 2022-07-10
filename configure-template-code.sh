4f61ab6f72fec7edf0450b744eebe5d5db2b9bd6
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-07-10 03:48:22 -0300

    Adiciona o arquivo configure-template-code.sh

---
+++ configure-template-code.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+
+function make_clean() {
+    local path=$1
+    local control=${path}/w/c
+    cd "${control}" && make clean
+}
+
+function make_() {
+    local path=$1
+    local control=${path}/w/c
+    cd "${control}" && make
+}
+
+function mk_code() {
+    local path=$1
+    local model=${path}/w/m
+    cd "${model}" && ./mk-code.sh
+}
+
+function mk_models() {
+    local path=$1
+    local view=${path}/w/v
+    cd "${view}" && {
+        test -d models && rm -rf models;
+        ./mk-models.sh;
+    }
+
+}
+##############################################################################
+
+function mvc_model() {
+    local w=$1
+    mk_code $w
+}
+
+function mvc_controller() {
+    local w=$1
+    make_clean $w
+    make_ $w
+}
+
+function mvc_view() {
+    local w=$1
+    mk_models $w
+}
+
+
+function mvc() {
+    local d=$1
+    mvc_model $d
+    mvc_controller $d
+    mvc_view $d
+}
+
+export PYENV_VERSION=neovim3
+#source ${HOME}/developer/qutebrowser/.venv/bin/activate
+# DO STUFF
+d=$(brew --prefix)/opt/template-code
+mvc ${d}/w-new-file
+
+# Reset version
+unset PYENV_VERSION
+
